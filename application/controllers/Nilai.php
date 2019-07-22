70a61d0bdbf5c5bdac8d3abf08b02773b4ebb213
samawacreative <samawadeveloper@gmail.com>
2019-07-22 21:19:22 +0700

    Comparison Table Nilai Create Table Comparison and Sum Data Rows

--- application/controllers/Nilai.php
+++ application/controllers/Nilai.php
@@ -15,11 +15,102 @@ class Nilai extends CoreModules {
 	{
       $acuan = $this->Mod->get_list_acuan();
 
-      $data['List'] = $acuan;
+      $data['TableJenisBahan'] = $this->get_compiled_table(1);
+      $data['TableBenang'] = $this->get_compiled_table(2);
+
       $data['_Breadcrumb'] = _Breadcrumb(array(1=>'Dashboard',2=>'Bobot Nilai'));
       $data['LoadScripts'] = _LoadJS(array('bobot/nilai'));
       $this->template->DisplayView('dashboard','app/bobot.nilai',$data);
    }
+
+
+
+   function get_compiled_table($id_kriteria){
+      $HTML_HEADER = $this->get_row_header($id_kriteria);
+      $HTML_CONTENT = $this->get_row_content($id_kriteria);
+      $HTML_TOTAL = $this->get_row_total($id_kriteria);
+      $HTML = '<table id="table-data" class="table table-striped table-bordered" style="text-align:center;">';
+      $HTML.= $HTML_HEADER;
+      $HTML.= $HTML_CONTENT;
+      $HTML.= $HTML_TOTAL;
+      $HTML.= '</table>';
+      return $HTML;
+   }
+
+   function get_row_header($id_kriteria){
+      $cKriteria = $this->App->_GetTableData('data_kriteria', array('id_kriteria' => $id_kriteria ));
+      $cList = $this->Mod->get_kain();
+
+      $HTML ='';
+      $HTML .='<tr>';
+      // $HTML .='<td style="font-size:11px">'.$cVal.'</td>';
+      $HTML .='<td style="font-size:11px">'.$cKriteria->nm_kriteria.'</td>';
+      foreach ($cList as $kain) {
+         $HTML .='<td style="font-size:11px">'.$kain->nm_kain.'</td>';
+      }
+      $HTML .='</tr>';
+      return $HTML;
+   }
+
+   function get_row_content($id_kriteria){
+      
+      $cList = $this->Mod->get_bobot($id_kriteria);
+      $row = array();
+      foreach ($cList as $key) {
+         $row['item'][$key->id_kain][$key->id_kain_to] = $key->nilai_bobot;
+         $row['parent'][$key->id_kain] = $key->nm_kain;
+      }
+      
+      
+      $HTML= '';
+      foreach ($row['parent'] as $cRow => $cVal) {
+         
+         $HTML.= '<tr>';
+         $HTML.='<td style="font-size:11px">'.$cVal.'</td>';
+         foreach ($row['item'] as $cItemKey => $cItemVal) {
+            // echo '<td>'.$row['item'][$cRow][$cItemKey].'</td>';
+            $HTML.='<td><input class="form-control-sm form-control input-sm" type="text" id="inText" name="bobot[]" value="'.$row['item'][$cRow][$cItemKey].'"></td>';
+         }
+         $HTML.='</tr>';
+      }
+      return $HTML;
+      // return $data;
+   }
+   
+   function get_row_total($id_kriteria=null){
+      $cList = $this->Mod->get_bobot($id_kriteria);
+      $row = array();
+      foreach ($cList as $key) {
+         $row['item'][$key->id_kain][$key->id_kain_to] = $key->nilai_bobot;
+         $row['parent'][$key->id_kain] = $key->nm_kain;
+      }
+      
+      $HTML= '';
+      $HTML.= '<tr>';
+      $HTML.='<td style="font-size:11px">Jumlah</td>';
+      $cData = array();
+      foreach ($row['parent'] as $cRow => $cVal) {
+         $cData[] = $row['item'][$cRow];   
+      }
+      
+      $sum = array();
+      foreach ($cData as $key => $sub_array) {
+         foreach ($sub_array as $sub_key => $value) {
+            if( ! array_key_exists($sub_key, $sum)) $sum[$sub_key] = 0;
+            $sum[$sub_key]+=$value;
+         }
+         
+      }
+      foreach ($sum as $key => $value) {
+         $HTML.='<td><input class="form-control-sm form-control input-sm" type="text" id="inText" name="bobot[]" value="'.$sum[$key].'"></td>';            
+      }
+      // var_dump($sum);
+      
+      $HTML.= '</tr>';
+      return $HTML;
+   }
+
+
    
    public function forms(){
       $data['_Breadcrumb'] = _Breadcrumb($this->uri->segment_array());
--- application/models/NilaiModel.php
+++ application/models/NilaiModel.php
@@ -8,8 +8,8 @@ class NilaiModel extends CI_Model {
    public function query(){
       $this->db->select('*')
                ->from($this->table);
-   }
-
+   }   
+   
    public function get_list_acuan(){
       $this->query();
       $data = $this->db->get(); 
@@ -18,6 +18,54 @@ class NilaiModel extends CI_Model {
       }else{
          return false;
       }       
+   }
+   
+   private function query_nilai_bobot(){
+      $this->db->select('
+                  data_bobot.id_bobot,
+                  data_kain.nm_kain,
+                  data_bobot.id_kain,
+                  data_bobot.id_kain_to,
+                  data_bobot.id_kriteria,
+                  data_kriteria.nm_kriteria,
+                  data_bobot.nilai_bobot')
+               ->from('data_bobot')
+               ->join('data_kriteria','data_kriteria.id_kriteria = data_bobot.id_kriteria')
+               ->join('data_kain','data_kain.id_kain = data_bobot.nilai_bobot');
+   }
+
+   public function get_bobot($kriteria=null){
+      // $this->query_nilai_bobot();
+      $this->db->where('id_kriteria',$kriteria);
+      $data = $this->db->get('bobot_by_kain');
+      if($data->num_rows() > 0 ){
+         return $data->result();
+      }else{
+         return false;
+      }     
+   }
+
+
+   public function query_kain(){
+      $this->db->select('*')
+               ->from('data_kain');
+   }
+
+   public function get_kain(){
+      $this->query_kain();
+      $data = $this->db->get();
+      if($data->num_rows() > 0 ){
+         return $data->result();
+      }else{
+         return false;
+      }     
+   }
+
+
+
+   private function query_kriteria(){
+      $this->db->select('*')
+               ->from('data_kriteria');
    }
 
 
